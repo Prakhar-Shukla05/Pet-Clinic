5bc6fc3766c9d394428c2423ea48d86e9bae95d3
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-03-14 02:11:50 +0530

    Refactored Map Implementation enable abstract ID genration. Closes #24

--- pet-clinic-data/src/main/java/com/prakhar/petclinic/services/map/AbstractMapClass.java
+++ pet-clinic-data/src/main/java/com/prakhar/petclinic/services/map/AbstractMapClass.java
@@ -1,13 +1,12 @@
 package com.prakhar.petclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.prakhar.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapClass<T,ID> {
+import java.util.*;
 
-    protected Map<ID,T> map = new HashMap<>();
+public abstract class AbstractMapClass<T extends BaseEntity,ID extends Long> {
+
+    protected Map<Long,T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -16,8 +15,17 @@ public abstract class AbstractMapClass<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id,object);
+    T save(T object){
+
+        if(object!=null){
+            if(object.getId()==null){
+                object.setId(getNextID());
+            }
+            map.put(object.getId(),object);
+        }else{
+            throw new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
     void deleteById(ID id){
@@ -27,4 +35,19 @@ public abstract class AbstractMapClass<T,ID> {
         map.entrySet().removeIf(entry->entry.getValue().equals(object));
     }
 
+    private Long getNextID(){
+        Long nextId= null;
+
+        try{
+            nextId=Collections.max(map.keySet())+1;
+        }
+        catch (NoSuchElementException e){
+            nextId=1L;
+        }
+
+        return nextId;
+
+    }
+
+     
 }
--- pet-clinic-data/src/main/java/com/prakhar/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/prakhar/petclinic/services/map/VetServiceMap.java
@@ -20,7 +20,7 @@ public class VetServiceMap extends  AbstractMapClass<Vet,Long> implements VetSer
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(), object);
+        return super.save(object);
     }
 
     @Override
