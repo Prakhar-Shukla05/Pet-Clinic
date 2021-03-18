4d3cc5e8ae4bd7e25a7b603934ee461f46d7a613
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-03-18 17:32:32 +0530

    Added visit. Update owner with pet relationship and pet type with setters and getters . Closes #27

--- pet-clinic-data/src/main/java/com/prakhar/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/prakhar/petclinic/model/PetType.java
@@ -1,6 +1,13 @@
 package com.prakhar.petclinic.model;
 
 public class PetType extends BaseEntity{
-    String name;
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
