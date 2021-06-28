54f45d51a1c783a0e9483078d7896c7b1c49c707
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-06-28 15:37:41 +0530

    Fixed the menu links. Closes #48

--- pet-clinic-web/src/main/java/com/prakhar/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/prakhar/petclinic/controllers/OwnerController.java
@@ -23,4 +23,10 @@ public class  OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping({"/find","/find/",})
+    public String findOwners(){
+        return "notimplemented";
+    }
+
+
 }
