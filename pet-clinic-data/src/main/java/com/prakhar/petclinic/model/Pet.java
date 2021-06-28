5d586dd4f4d0d2e6c10891b91febf2b736c88366
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-06-28 22:12:23 +0530

    Created Visit Entity. Closes #39

--- pet-clinic-data/src/main/java/com/prakhar/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/prakhar/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.prakhar.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name= "pets")
@@ -21,8 +23,20 @@ public class Pet extends BaseEntity{
     @Column(name ="birth_date")
     private LocalDate birthDate;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits= new HashSet<>();
+
+
     public String getName() {
         return name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     public void setName(String name) {
--- pet-clinic-data/src/main/java/com/prakhar/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/prakhar/petclinic/model/Visit.java
@@ -1,11 +1,23 @@
 package com.prakhar.petclinic.model;
 
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name ="visits")
 public class Visit extends BaseEntity {
 
+    @Column(name= "date")
     private LocalDate date;
+
+    @Column(name ="description")
     private String description;
+
+
+    @ManyToOne
+    @JoinColumn(name= "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
